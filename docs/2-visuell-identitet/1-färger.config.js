e92a3e5508600fdd7e7650a8a34f5e2004014950
Anton Niklasson <niklasson.anton@gmail.com>
2018-12-13 16:18:15 +0100

    update the colors page

--- docs/2-visuell-identitet/1-färger.config.js
+++ docs/2-visuell-identitet/1-färger.config.js
@@ -1,65 +1,119 @@
 module.exports = {
   context: {
-    colors: [
+    colorGroups: [
       {
-        name: "Textfärg",
-        hex: "#292929",
-        variable: "$color-text"
+        title: "Profilfärger",
+        description: 'Skolverkets primära färger är lile och mörkturkos. Dessa ska användas genomgående i all kommunikation.',
+        colors: [
+          {
+            name: "Profilfärg 1",
+            hex: "#692859",
+          },
+          {
+            name: "Profilfärg 2",
+            hex: "#00424C",
+          }
+        ]
       },
       {
-        name: "Sekundär textfärg",
-        hex: "#666666",
-        variable: "$color-text-secondary"
+        title: "Accentfärger",
+        description: 'Accentfärgerna används sparsamt. Används främst till dekorelement och ikoner.',
+        colors: [
+          {
+            name: "Accentfärg 1",
+            hex: "#99CED4",
+          },
+          {
+            name: "Accentfärg 2",
+            hex: "#EF7748",
+
+          },
+          {
+            name: "Accentfärg 3",
+            hex: "#F59C00",
+          }
+        ]
       },
       {
-        name: "Ljus textfärg",
-        hex: "#FFFFFF",
-        variable: "$color-text-light"
+        title: "Bakgrundsfärger",
+        description: 'Bakgrundsfärgerna får även användas i 50% och 25% opacitet.',
+        previewProperty: 'backgroundColor',
+        colors: [
+          {
+            name: "Bakgrundsfärg 1",
+            hex: "#DCEAEA",
+            variants: [
+              {
+                label: '50%',
+                hex: "#EDF4F4",
+              },
+              {
+                label: '25%',
+                hex: "#F6FAFA",
+              }
+            ]
+          },
+          {
+            name: 'Bakgrundsfärg 2',
+            hex: "#FCDBCB",
+            variants: [
+              {
+                label: '50%',
+                hex: "#FDEDE5",
+              },
+              {
+                label: '25%',
+                hex: "#FEF6F2",
+              }
+            ]
+          },
+          {
+            name: 'Bakgrundsfärg 3',
+            hex: '#ffe56f',
+            variants: [
+              {
+                label: '50%',
+                hex: '#FFF2B7',
+              },
+              {
+                label: '25%',
+                hex: '#FFF8DB',
+              }
+            ]
+          }
+        ]
       },
       {
-        name: "Länkfärg",
-        hex: "#0079C2",
-        variable: "$color-link"
+        title: "Komplementfärger",
+        description: 'Komplementfärgerna används vid behov av ytterligare färger, exempelvis i diagram och grafik.',
+        colors: [
+          {
+            name: "Komplementfärg 1",
+            hex: "#497E89",
+          },
+          {
+            name: 'Komplementfärg 2',
+            hex: "#B1451C",
+          },
+        ]
       },
       {
-        name: "Länkfärg, hover",
-        hex: "#005182",
-        variable: "$color-link-hover"
-      },
-      {
-        name: "Länkfärg, besökt",
-        hex: "#386406",
-        variable: "$color-link-visited"
-      },
-      {
-        name: "Ramfärg",
-        hex: "#E1D4E1",
-        variable: "$color-border"
-      },
-      {
-        name: "Bakgrund, mörk",
-        hex: "#692969",
-        variable: "$color-background-dark"
-      },
-      {
-        name: "Bakgrund, ljus",
-        hex: "#F8F4F8",
-        variable: "$color-background-light"
-      },
-      {
-        name: "Fel",
-        hex: "#CD2126",
-        variable: "$color-error"
-      },
-      {
-        name: "Fokus",
-        hex: "#FCB933",
-        variable: "$color-focus"
-      },
-      {
-        name: "Turkos",
-        hex: "#178079",
-        variable: "$color-turkos"
+        title: "Ramverk",
+        colors: [
+          {
+            name: "Textfärg",
+            hex: "#292929",
+            textColor: 'white',
+          },
+          {
+            name: "Länkfärg",
+            hex: "#0079C2",
+          },
+          {
+            name: 'Felmeddelande',
+            hex: "#CD2126"
+          }
+        ]
       }
     ]
   }
