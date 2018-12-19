c9bbcaf7ee69bfbc9c5611f32e7e3f6c78748513
Anton Niklasson <niklasson.anton@gmail.com>
2018-12-19 14:45:15 +0100

    add navigation bar component

---
+++ src/ui/components/navigation-bar/navigation-bar.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: 'Många frågor om...',
+  handle: 'navigation-bar',
+  context: {
+    title: 'Många frågar om...',
+    links: [
+      {
+        label: 'Sit Amet Ipsum',
+        url: '#'
+      },
+      {
+        label: 'Lorem Dolor Sit Amet Lorem Dolor Sit Amet Dolor',
+        url: '#'
+      },
+      {
+        label: 'Lorem Dolor',
+        url: '#'
+      },
+      {
+        label: 'Lorem Dolor',
+        url: '#'
+      },
+    ]
+  }
+}
