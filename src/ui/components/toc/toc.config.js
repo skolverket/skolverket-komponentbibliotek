be72f0c8c87f8b457e4fbb398d2ea4bfec6e68fe
Isac Lagerblad <isac.lagerblad@gmail.com>
2019-01-23 10:44:01 +0100

    Updated TOC with better example

--- src/ui/components/toc/toc.config.js
+++ src/ui/components/toc/toc.config.js
@@ -5,16 +5,61 @@ module.exports = {
     title: 'Innehåll på denna sida',
     headings: [
       {
-        title: '1 augusti 2018',
-        url: '#h-1augusti2018'
+        url: '#h-Darforskaskolorochforskolorarbetamedsakerhetochkrisberedskap',
+        title:
+          'Därför ska skolor och förskolor arbeta med säkerhet och krisberedskap'
       },
       {
-        title: '1 januari 2019',
-        url: '#h-1januari2019'
+        url: '#h-Arbetasystematisktmedsakerheten',
+        title: 'Arbeta systematiskt med säkerheten'
       },
       {
-        title: '1 juli 2019',
-        url: '#h-1juli2019'
+        url: '#h-Forebyggaincidentersomkanledatillkriser',
+        title: 'Förebygga incidenter som kan leda till kriser'
+      },
+      {
+        url: '#h-Okakrisberedskapen',
+        title: 'Öka krisberedskapen'
+      },
+      {
+        url: '#h-Utbildningarochmaterial',
+        title: 'Utbildningar och material'
+      },
+      {
+        url: '#h-Statsbidragforsakerhetshojandeatgarder2018',
+        title: 'Statsbidrag för säkerhetshöjande åtgärder 2018'
+      },
+      {
+        url: '#h-Setillattallaharkannedomomlokalerna',
+        title: 'Se till att alla har kännedom om lokalerna'
+      },
+      {
+        url: '#h-Allpersonalskavetahurlarmetfungerar',
+        title: 'All personal ska veta hur larmet fungerar'
+      },
+      {
+        url: '#h-Utarbetarutinerforkommunikation',
+        title: 'Utarbeta rutiner för kommunikation'
+      },
+      {
+        url: '#h-Ovakrisplanenmedpersonalen',
+        title: 'Öva krisplanen med personalen'
+      },
+      {
+        url: '#h-Olikahandlingsalternativvidvapnatvald',
+        title: 'Olika handlingsalternativ vid väpnat våld'
+      },
+      {
+        url: '#h-Videnakutsituation',
+        title: 'Vid en akut situation'
+      },
+      {
+        url: '#h-Ansvarochbefogenheteriskolforfattningarna',
+        title: 'Ansvar och befogenheter i skolförfattningarna'
+      },
+      {
+        url: '#h-Diskussionsfragor',
+        title: 'Diskussionsfrågor'
       }
     ]
   }
