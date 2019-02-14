3c697b21837029017e579a2912c5564e97d3b080
Anton Niklasson <niklasson.anton@gmail.com>
2019-02-14 15:31:33 +0100

    decouple the news list from content navigation, introduce nav-link component

---
+++ src/ui/components/nav-link/nav-link.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'Länk',
+  handle: 'nav-link',
+  context: {
+    href: '#',
+    label: 'Lorem ipsum dolor sit amet lorem ipsum dolor sit amet',
+    icon: 'chevron-right'
+  },
+  variants: [
+    {
+      name: 'Extern',
+      context: {
+        label: 'Navigera till extern sida',
+        icon: 'external'
+      }
+    }
+  ]
+}
--- src/ui/containers/news/news.config.js
+++ src/ui/containers/news/news.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   name: 'Nyhetslista',
-  handle: 'news-list'
+  handle: 'news-list',
+  context: {
+    title: 'Nyhetslista',
+    items: [
+      {
+        title: 'Lorem ipsum dolor sit amet',
+        date: '29 maj 2018'
+      },
+      {
+        title: 'Amet sit dolor ipsum lorem',
+        date: '24 december 2018'
+      }
+    ]
+  }
 }
