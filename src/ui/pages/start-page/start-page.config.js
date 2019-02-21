34212fc91b1fbacd6bfd897217036d6a0e3c1956
Anton Niklasson <niklasson.anton@gmail.com>
2019-02-21 12:06:12 +0100

    [SVVT19-59] Nyhetslista (#79)

--- src/ui/pages/start-page/start-page.config.js
+++ src/ui/pages/start-page/start-page.config.js
@@ -5,6 +5,53 @@ module.exports = {
   context: {
     heroModel: {
       title: 'Gå webbkursen om fritidshemmets uppdrag'
+    },
+    news: {
+      title: 'Antons nyheter',
+      items: [
+        {
+          date: '1 april 2019',
+          link: {
+            label: 'April april!',
+            url: 'april-april'
+          }
+        },
+        {
+          date: '2 februari 2019',
+          link: {
+            label: 'Snart är det sommar!',
+            url: 'snart-sommar'
+          }
+        },
+        {
+          date: '31 december 2018',
+          link: {
+            label: 'Gott nytt år!',
+            url: 'gott-nytt-ar'
+          }
+        },
+        {
+          date: '24 december 2018',
+          link: {
+            label: 'Amet sit dolor ipsum lorem',
+            url: 'amet-sit-dolor-ipsum-lorem'
+          }
+        },
+        {
+          date: '29 maj 2018',
+          link: {
+            label: 'Lorem ipsum dolor sit amet',
+            url: 'lorem-ipsum-dolor-sit-amet',
+          }
+        },
+        {
+          date: '14 april',
+          link: {
+            label: 'Glad påsk!',
+            url: 'glad-pask'
+          }
+        }
+      ]
     }
   }
 }
