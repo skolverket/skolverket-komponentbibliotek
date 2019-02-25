b6ba1aaebeea87a5023ad972d982cce73c938adc
Isac Lagerblad <icaaaq@gmail.com>
2019-02-25 14:54:41 +0100

    Updated navigation (#82)
    * focus improvements for the site header
    
    * improving focus management slightly for buttons
    
    * show focus rings only on explicit keyboard navigation
    
    * update buttons and section navigation according to global focus management
    
    * improving focus management in the site footer slightly
    
    * use nav-link in site footer
    
    * add focus backgruond to radiobuttons and checkboxes
    
    * remove test data from footer context
    
    * hide native focus outlines in box-link and card
    
    * Updated navigation
    
    * Added a-o icon, added a-o to header.config and fixed focus style

--- src/ui/framework/main.js
+++ src/ui/framework/main.js
@@ -1,6 +1,8 @@
 require('../core/js/accessible-link');
+require('../core/js/focus-management');
 
 require('../components/accordion/accordion');
 require('../components/reveal/reveal');
 require('../components/toc/toc');
 require('../framework/header/header');
+require('../framework/navigation/navigation');
