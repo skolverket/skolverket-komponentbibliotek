79333488215649e97f91e5aaf5088746745e7c21
Anton Niklasson <niklasson.anton@gmail.com>
2019-02-28 15:08:23 +0100

    [SVVT19-22] Målgruppssida (#83)
    * add targeted-page
    
    * fix spacing and add more components
    
    * fix some spacing and make the title and ingress more responsive

---
+++ src/ui/pages/targeted-page/readme.md
@@ -0,0 +1,17 @@
+# Målgruppssida
+
+## För redaktörer
+
+*Hur förklarar vi sidmallar för redaktörer?*
+
+## För utvecklare
+
+Mallen för målgruppssidan kommer med ett par fördefinierade komponenter i toppytan, men fungerar i övrigt som en vanlig sida nere
+i boxen.
+
+| Element     | Beskrivning                            |
+|-------------|----------------------------------------|
+| Rubrik      | En stor rubrik för hela sidan          |
+| Ingress     | Inledande paragraf för sidans innehåll |
+
+Se kontextobjektet för närmare detaljer.
---
+++ src/ui/pages/targeted-page/targeted-page.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  name: 'Målgruppssida',
+  handle: 'targeted-page',
+  preview: '@layout--page',
+  context: {
+    title: 'Skolbibliotekarie',
+    ingress: 'Som skolbibliotekarie kan du vara en resurs i undervisning och skolutveckling. Här finns stöd för ditt arbete.',
+    linkList: {
+      title: "Ta mig till...",
+      isOutlined: true,
+      links: [
+        {
+          text: "Lorem Ipsum",
+          url: "#",
+        },
+        {
+          text: "Beställ nya böcker",
+          url: "#",
+        },
+        {
+          text: "Dolor Sit Amet",
+          url: "#",
+        }
+      ]
+    },
+    contentNav: {
+      title: 'Stöd för skolbibliotek',
+      columnOne: [
+        { label: 'Planer, utvärdera och genomföra' },
+        { label: 'Stärk elevers språkliga förmåga' }
+      ],
+      columnTwo: [
+        { label: 'Skapa skolbibliotek för alla' },
+        { label: 'Stärk elevers digitala kompetens' }
+      ]
+    }
+  }
+}
