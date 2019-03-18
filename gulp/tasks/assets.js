42bcc4035a2fd61968ef8c96b13f3a5f9d207e98
Isac Lagerblad <isac.lagerblad@gmail.com>
2019-03-18 13:54:41 +0100

    Moved dist and site to build folder

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,40 +1,44 @@
-const del = require('del')
-const gulpSequence = require('gulp-sequence')
-const flatten = require('gulp-flatten')
-const gulpUtils = require('../gulp-utils')
+const del = require('del');
+const gulpSequence = require('gulp-sequence');
+const flatten = require('gulp-flatten');
+const gulpUtils = require('../gulp-utils');
 
-const rootSource = 'src/ui/*'
-const uiSource = ['src/ui/assets/**/*', '!src/ui/assets/svg', '!src/ui/assets/svg/**/*']
-const devImgSource = 'src/dev/img/**/*'
+const rootSource = 'src/ui/*';
+const uiSource = [
+  'src/ui/assets/**/*',
+  '!src/ui/assets/svg',
+  '!src/ui/assets/svg/**/*'
+];
+const devImgSource = 'src/dev/img/**/*';
 
 module.exports = gulp => {
-
   gulp.task('assets:clean', () => {
-    return del('dist/assets')
-  })
+    return del('build/dist/assets');
+  });
 
   gulp.task('assets:root', () => {
-    return gulp.src(rootSource).pipe(flatten()).pipe(gulp.dest('dist'))
-  })
+    return gulp
+      .src(rootSource)
+      .pipe(flatten())
+      .pipe(gulp.dest('build/dist'));
+  });
 
   gulp.task('assets:ui', () => {
-    return gulp.src(uiSource).pipe(gulp.dest('dist/assets'))
-  })
+    return gulp.src(uiSource).pipe(gulp.dest('build/dist/assets'));
+  });
 
   gulp.task('assets:devImg', () => {
-    return gulp.src(devImgSource).pipe(gulp.dest('dist/tmp/img'))
-  })
+    return gulp.src(devImgSource).pipe(gulp.dest('build/dist/tmp/img'));
+  });
 
   gulp.task('assets:all', callback => {
-    gulpSequence(['assets:root', 'assets:ui', 'assets:devImg'])(callback)
-  })
+    gulpSequence(['assets:root', 'assets:ui', 'assets:devImg'])(callback);
+  });
 
   gulp.task('assets:watch', () => {
     gulp.watch([rootSource, uiSource, devImgSource], event => {
-      gulpUtils.logWatchEvent(event)
-      gulpSequence('assets:clean', 'assets:all')(gulpUtils.mapError)
-    })
-  })
-
-
-}
+      gulpUtils.logWatchEvent(event);
+      gulpSequence('assets:clean', 'assets:all')(gulpUtils.mapError);
+    });
+  });
+};
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,10 +1,10 @@
-const gulp = require('gulp')
-const source = require('vinyl-source-stream')
-const buffer = require('vinyl-buffer')
-const uglify = require('gulp-uglify')
-const browserify = require('browserify')
-const watchify = require('watchify')
-const gulpUtils = require('../gulp-utils')
+const gulp = require('gulp');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const gulpUtils = require('../gulp-utils');
 
 const _scripts = [
   {
@@ -12,7 +12,7 @@ const _scripts = [
     watch: ['src/ui/**/*.js'],
     fileName: 'main'
   }
-]
+];
 
 const browserifyConfig = entry => {
   return {
@@ -23,37 +23,34 @@ const browserifyConfig = entry => {
     packageCache: {},
     transform: ['babelify'],
     plugin: gulpUtils.isDevelopment() ? [watchify] : []
-  }
-}
+  };
+};
 
 const bundle = (bw, fileName) => {
-
   bw.bundle()
     .on('error', gulpUtils.mapError)
     .pipe(source(`${fileName}.min.js`))
     .pipe(buffer())
     .pipe(gulpUtils.isProduction(uglify()))
-    .pipe(gulp.dest('dist/js'))
-}
+    .pipe(gulp.dest('build/dist/js'));
+};
 
 module.exports = gulp => {
-
   gulp.task('scripts:all', () => {
-
     _scripts.map(script => {
-      script.cache = script.cache || browserify(browserifyConfig(script.entry))
+      script.cache = script.cache || browserify(browserifyConfig(script.entry));
 
       if (gulpUtils.isProduction() || gulpUtils.useEPiServer) {
-        script.cache.ignore('./src/dev/api/adapter.js')
+        script.cache.ignore('./src/dev/api/adapter.js');
       }
 
       if (gulpUtils.isDevelopment) {
-        script.cache.on('update', () => { bundle(script.cache, script.fileName) });
+        script.cache.on('update', () => {
+          bundle(script.cache, script.fileName);
+        });
       }
 
-      bundle(script.cache, script.fileName)
-    })
-
-  })
-
-}
+      bundle(script.cache, script.fileName);
+    });
+  });
+};
