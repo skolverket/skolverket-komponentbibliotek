94f98bf31211e4f492bc7e6b5af2b382c0021390
Isac Lagerblad <isac.lagerblad@gmail.com>
2019-03-18 15:24:01 +0100

    structured reveal component

--- src/ui/components/reveal/reveal.config.js
+++ src/ui/components/reveal/reveal.config.js
@@ -2,13 +2,13 @@ module.exports = {
   name: 'Visa/Dölj',
   handle: 'reveal',
   status: 'done',
+  default: 'Standard',
   context: {
     id: 1321,
     title: 'Lorem ipsum dolor sit amet',
     toggleCloseText: 'Dölj',
     toggleOpenText: 'Visa',
-    expanded: false,
-    body: 'rich-text',
+    expanded: false
   },
   variants: [
     {
@@ -17,9 +17,10 @@ module.exports = {
     },
     {
       name: 'expanded',
+      label: 'Utfälld',
       context: {
         expanded: true
       }
     }
   ]
-}
+};
