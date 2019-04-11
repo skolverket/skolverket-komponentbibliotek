bbc0b961d0423a4f5429527ca78902a0f80ec8d7
Isac Lagerblad <isac.lagerblad@gmail.com>
2019-04-11 15:05:17 +0200

    Simplified the archive builder and corrected the content.

--- scripts/release-it/buildReleaseArchive.js
+++ scripts/release-it/buildReleaseArchive.js
@@ -1,58 +1,25 @@
 #! /usr/bin/env node
 
-const path = require('path');
 const fs = require('fs');
-const zip = new require('node-zip')();
+const archiver = require('archiver');
 const pkg = require('../../package.json');
 
-const archiveName = `skolverket-bundle-${pkg.version}`;
-const files = {
-  './build/dist/js/main.min.js': '/js/main.min.js',
-  './build/dist/css/main.min.css': '/css/main.min.css',
-  './build/dist/css/no-js.min.css': '/css/no-js.min.css',
-  './build/dist/assets/': 'assets/',
-  'README.md': 'README.md'
-};
+var output = fs.createWriteStream(`skolverket-bundle-${pkg.version}.zip`);
+var archive = archiver('zip');
 
-const isDirectory = name => {
-  return fs.lstatSync(name).isDirectory();
-};
-
-const getSubtree = (folder, alias) => {
-  let tree = {};
-  const files = fs.readdirSync(folder);
-
-  files.forEach(file => {
-    const filepath = path.join(folder, file);
-
-    if (isDirectory(filepath)) {
-      const subtree = getSubtree(filepath, filepath.replace(folder, alias));
-      tree = { ...tree, ...subtree };
-    } else {
-      tree[filepath.replace(folder, alias)] = fs.readFileSync(filepath);
-    }
-  });
-
-  return tree;
-};
-
-Object.entries(files).forEach(([source, destination]) => {
-  if (isDirectory(source)) {
-    const subtree = getSubtree(source, destination);
-
-    Object.entries(subtree).forEach(([filepath, data]) => {
-      zip.file(path.join(archiveName, filepath), data);
-    });
-  } else {
-    const sourceData = fs.readFileSync(
-      path.resolve(__dirname, '../..', source)
-    );
-    zip.file(path.join(archiveName, destination), sourceData);
-  }
+output.on('close', function() {
+  console.log(archive.pointer() + ' total bytes');
+  console.log(
+    'archiver has been finalized and the output file descriptor has closed.'
+  );
 });
 
-fs.writeFileSync(
-  `${archiveName}.zip`,
-  zip.generate({ base64: false, compression: 'DEFLATE' }),
-  'binary'
-);
+archive.on('error', function(err) {
+  throw err;
+});
+
+archive.pipe(output);
+
+archive.directory('./build/dist/', false);
+
+archive.finalize();
