81c47c3f5a38f27809131d8865ee0007a58c9d78
Isac Lagerblad <icaaaq@gmail.com>
2019-04-11 07:31:19 +0200

    Added visited style color for box-links (#108)

--- src/ui/components/box-link/box-link.config.js
+++ src/ui/components/box-link/box-link.config.js
@@ -2,26 +2,26 @@ const linkTypes = {
   internal: 'internal',
   external: 'external',
   anchor: 'anchor'
-}
+};
 
 const layouts = {
   imageLink: 'imageLink',
   textLink: 'textLink'
-}
+};
 
 const patterns = {
   greenSquares: 'green-squares',
   greenWaves: 'green-waves',
   purpleSquares: 'purple-squares',
   purpleWaves: 'purple-waves'
-}
+};
 
 module.exports = {
   name: 'Boxlänk',
   handle: 'box-link',
   status: 'done',
   context: {
-    url: '#',
+    url: 'https://www.skolverket.se/',
     linkType: linkTypes.internal,
     layout: layouts.imageLink,
     pattern: null,
@@ -29,7 +29,8 @@ module.exports = {
       src: '/styleguide/tmp/img/box-links/karlbergs_skola_fritidshem',
       alt: 'Karlbergs skola fritidshem'
     },
-    headline: 'Digitalisering av de nationella proven genom successivt införande',
+    headline:
+      'Digitalisering av de nationella proven genom successivt införande',
     teaser: null
   },
   variants: [
@@ -46,7 +47,8 @@ module.exports = {
         layout: layouts.textLink,
         pattern: patterns.greenSquares,
         image: null,
-        teaser: 'Vi arbetar för att digitalisera de nationella proven och möjliggöra extern bedömning. Redan från hösten 2018 ska skolorna använda <strong>digitala enheter</strong> vid vissa prov'
+        teaser:
+          'Vi arbetar för att digitalisera de nationella proven och möjliggöra extern bedömning. Redan från hösten 2018 ska skolorna använda <strong>digitala enheter</strong> vid vissa prov'
       }
     },
     {
@@ -55,7 +57,8 @@ module.exports = {
         layout: layouts.textLink,
         pattern: patterns.greenWaves,
         image: null,
-        teaser: 'Vi arbetar för att digitalisera de nationella proven och möjliggöra extern bedömning. Redan från hösten 2018 ska skolorna använda <strong>digitala enheter</strong> vid vissa prov'
+        teaser:
+          'Vi arbetar för att digitalisera de nationella proven och möjliggöra extern bedömning. Redan från hösten 2018 ska skolorna använda <strong>digitala enheter</strong> vid vissa prov'
       }
     },
     {
@@ -64,7 +67,8 @@ module.exports = {
         layout: layouts.textLink,
         pattern: patterns.purpleSquares,
         image: null,
-        teaser: 'Vi arbetar för att digitalisera de nationella proven och möjliggöra extern bedömning. Redan från hösten 2018 ska skolorna använda <strong>digitala enheter</strong> vid vissa prov'
+        teaser:
+          'Vi arbetar för att digitalisera de nationella proven och möjliggöra extern bedömning. Redan från hösten 2018 ska skolorna använda <strong>digitala enheter</strong> vid vissa prov'
       }
     },
     {
@@ -73,8 +77,9 @@ module.exports = {
         layout: layouts.textLink,
         pattern: patterns.purpleWaves,
         image: null,
-        teaser: 'Vi arbetar för att digitalisera de nationella proven och möjliggöra extern bedömning. Redan från hösten 2018 ska skolorna använda <strong>digitala enheter</strong> vid vissa prov'
+        teaser:
+          'Vi arbetar för att digitalisera de nationella proven och möjliggöra extern bedömning. Redan från hösten 2018 ska skolorna använda <strong>digitala enheter</strong> vid vissa prov'
       }
     }
   ]
-}
+};
